2819eda7ff856fbd416e703c03f29e5be2ba2a5d
Mohammad96 <mohammadhasani9696@gmail.com>
2022-01-01 12:14:48 +0100

    Add project files.

---
+++ MonsterCardTradingGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonsterCardTradingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
